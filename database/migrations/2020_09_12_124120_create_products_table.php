244b85e530439b6d8cb45f2fef3acf4e08151b10
vagrant <vagrant@homestead>
2020-09-12 23:15:20 +0000

    add product model and migration (multiple table)

--- database/migrations/2020_09_12_124120_create_products_table.php
+++ database/migrations/2020_09_12_124120_create_products_table.php
@@ -15,6 +15,20 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('brand_id')->index();
+            $table->string('sku');
+            $table->string('name');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->unsignedInteger('quantity');
+            $table->decimal('weight', 8, 2)->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->decimal('sale_price', 8, 2)->nullable();
+            $table->boolean('status')->default(1);
+            $table->boolean('featured')->default(0);
+
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
